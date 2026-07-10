57d3ce6b887b9dcb5e2c2adfd29288a7e7273117
rogerswilcox63-sys <rogerswilcox63@gmail.com>
2026-07-10 06:35:21 +0100

    Add files via upload

--- config-template.js
+++ config-template.js
@@ -1,5 +1,5 @@
 // config-template.js - Safe to commit to GitHub
 window.API_CONFIG = {
   apiKey: "YOUR_DEEPSEEK_API_KEY_HERE",
-  baseURL: "https://deepseek.com"
+  baseURL: "https://api.deepseek.com"
 };
